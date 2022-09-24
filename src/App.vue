45d527cd7d9ce68eadf1a46952514eb126e451c3
rab1t <r.leroi@live.nl>
2022-09-24 22:52:03 +0200

    add backend url to frontend

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,24 @@
 <template>
-  <div class="addon">
-    <div class="card">
-      <div class="item">
-        <div class="title">Name</div>
-        <div class="value">{{state.person.fullName}}</div>
-      </div>
-      <div class="item">
-        <div class="title">Company</div>
-        <div class="value">{{state.company.name}}</div>
-      </div>
-      <div class="item">
-        <div class="title">Location</div>
-        <div class="value">{{state.person.location}}</div>
-      </div>
-      <div class="item">
-        <div class="title">LinkedIn</div>
-        <div class="value"><a :href="state.person.linkedin" target="_blank" title="LinkedIn">{{state.person.linkedin}}</a></div>
-      </div>
-      <div class="item">
-        <div class="title">Role</div>
-        <div class="value">{{state.person.role}}</div>
-      </div>
+  <div>
+    <div class="item">
+      <div class="title">Name</div>
+      <div class="value">{{state.data.fullName}}</div>
+    </div>
+    <div class="item">
+      <div class="title">Company</div>
+      <div class="value">{{state.data.companyName}}</div>
+    </div>
+    <div class="item">
+      <div class="title">Location</div>
+      <div class="value">{{state.data.location}}</div>
+    </div>
+    <div class="item">
+      <div class="title">LinkedIn</div>
+      <div class="value"><a :href="state.data.linkedin" target="_blank" title="LinkedIn">{{state.data.linkedin}}</a></div>
+    </div>
+    <div class="item">
+      <div class="title">Role</div>
+      <div class="value">{{state.data.role}}</div>
     </div>
   </div>
 </template>
@@ -28,25 +26,12 @@
 import { onMounted, reactive } from 'vue';
 
 const state = reactive({
-  person: {},
-  company: {},
+  data: {},
 })
 
-onMounted(() => {
-  state.person = {
-    fullName: 'Ramses Leroi',
-    email: 'r.leroi@live.nl',
-    location: 'Utrecht, Netherlands',
-    linkedin: 'https://linkedin.com/in/itbazen',
-    role: 'Engineering',
-  };
-  state.company = {
-    name: 'Trengo',
-    domain: 'trengo.com',
-    logo: 'https://logo.clearbit.com/trengo.com?size=100',
-    description: 'Discover Trengo, the omnichannel communication platform that unifies all messaging channels into one single view. Make customer communicatio…',
-    employees: '100',
-  };
+onMounted(async () => {
+  const res = await fetch(`https://trengo-clearbit.itbazen.workers.dev/${document.location.search}`);
+  state.data = await res.json();
 })
 </script>
 <style scoped>
@@ -56,8 +41,5 @@ onMounted(() => {
   }
   .value {
     color: rgb(112, 118, 123);
-  }
-  .card {
-    padding-bottom: 2rem;
   }
 </style>
